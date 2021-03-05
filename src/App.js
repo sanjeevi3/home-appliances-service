bb532304464f8aba0f908c7639f2b7189f20b2b0
sanjeevi3 <sanjeevi.developer@gmail.com>
2021-03-05 19:28:53 +0530

    include routing in our project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import LogIn from './pages/LogIn';
+import { Switch,BrowserRouter as Router,Route, Redirect } from "react-router-dom";
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container-fluid">
+      <header >
+        <div className="header">
+          <h4 className="text-center text-bold  text-white">Home Appliances Services</h4>
+        </div>
+        
       </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact><Redirect to="/login" /></Route>
+          <Route path="/login"><LogIn /></Route>
+          <Route path="/register"><Register /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
