3e7908b3f578ea88316f3cd65191158bfcbaf417
sanjeevi3 <sanjeevi.developer@gmail.com>
2021-03-12 13:58:38 +0530

    add back drop and menu toggle component

--- src/layout/AuthorizedLayout/index.jsx
+++ src/layout/AuthorizedLayout/index.jsx
@@ -1,14 +1,30 @@
+import {useState} from "react"
+import BackDrop from "../../Components/UI/BackDrop";
 import Header from "./Header";
 import NavigationItems from "./NavigationItems";
 
-const { Link, NavLink } = require("react-router-dom")
 
 const AuthorizedLayout=(props)=>{
+  const [state, setstate] = useState({sideDrawer:false,backDrop:false})
+const menuToggleClickHandler=()=>{
+  
+if(state.sideDrawer){
+  removeSideDrawer()
+}else{
+  setstate({sideDrawer:true,backDrop:true})
+}
+}
+const removeSideDrawer=()=>{
+  setstate({sideDrawer:false,backDrop:false})
+}
     return(
         <>
-      <Header />
-      <div className="col-md-2 col-8 px-1 navigation-block">
-        <nav><NavigationItems/></nav>
+      <Header menuToggleClickHandler={menuToggleClickHandler} />
+      {
+        state.backDrop &&<BackDrop click={removeSideDrawer}/>
+      }
+      <div className={`col-md-2 col-8 px-1 navigation-block ${state.sideDrawer?"opened":"closed"}`}>
+        <nav><NavigationItems removeSideDrawer={removeSideDrawer}/></nav>
         </div>
       
     
