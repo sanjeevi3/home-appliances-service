9bd28535f2a389d47e1d36263d3cc36019dd1262
sanjeevi3 <sanjeevi.developer@gmail.com>
2021-03-12 13:54:17 +0530

    add back drop and menu toggle component

--- src/layout/AuthorizedLayout/Header.jsx
+++ src/layout/AuthorizedLayout/Header.jsx
@@ -1,11 +1,17 @@
+import DrawerToggle from "./DrawerToggle";
 
 
-const Header=()=>{
+const Header=(props)=>{
     return(
-        <header >
-        <div className="header">
+        <header  >
+            <div className="row mx-0">
+            <DrawerToggle click={props.menuToggleClickHandler} />
+                <div className="col-10 col-md-12">
           <h4 className="text-center text-bold  text-white">Home Appliances Services</h4>
         </div>
+            </div>
+           
+        
         
       </header>
     )
